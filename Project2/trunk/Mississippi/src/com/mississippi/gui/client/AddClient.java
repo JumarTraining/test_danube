f1d44be549598b66a42ab6973b8ee99290472fcf
aporter <aporter@23e29aa1-da40-42ce-a7d5-73b51d036e42>
2015-12-01 15:37:43 +0000

    brocken

---
+++ Project2/trunk/Mississippi/src/com/mississippi/gui/client/AddClient.java
@@ -0,0 +1,26 @@
+package com.mississippi.gui.client;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class AddClient extends ClientGui{
+	public AddClient(){
+		super();
+		
+		enter.removeAll();
+		enter.addActionListener(new ActionListener(){
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				// TODO Auto-generated method stub
+				ArrayList<String> columns = new ArrayList<String>(Arrays.asList("Name","Adress","Phone","Email","FK_StaffID"));
+				ArrayList<String> values = new ArrayList<String>(Arrays.asList(nameBox.getText(),adressBox.getText(),phoneBox.getText(),emailBox.getText(),contactBox.getText()));
+				
+			}
+				
+		});
+	
+	}
+}
---
+++ Project2/trunk/Mississippi/src/com/mississippi/gui/client/EditClient.java
@@ -0,0 +1,33 @@
+package com.mississippi.gui.client;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.ResultSet;
+
+public class EditClient extends ClientGui{
+	EditClient(int id){
+		super();
+		ResultSet Rs=search("Client","ClientID",""+id);
+		
+		while(Rs.next()){
+			nameBox.setText(Rs.getString(Rs.findColumn("Name")));
+			adressBox.setText(Rs.getString(Rs.findColumn("Adress")));
+			phoneBox.setText(Rs.getString(Rs.findColumn("Phone")));
+			emailBox.setText(Rs.getString(Rs.findColumn("Email")));
+			contactBox.setText(Rs.getString(Rs.findColumn("FK_StaffID")));
+		}
+		
+		enter.removeAll();
+		enter.addActionListener(new ActionListener(){
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				// TODO Auto-generated method stub
+				String value="('"+nameBox.getText()+z+adressBox.getText()+z+phoneBox.getText()+z+emailBox.getText()+z+contactBox.getText()+"');";
+				String s = "INSERT INTO Cient (Name,Adress,Phone,Email,FK_StaffID) VALUES"+value;
+				
+			}
+				
+		});
+	}
+}
